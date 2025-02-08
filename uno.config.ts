59bba98da3f75a94e8876b6cc599c323f0aa894a
water2027 <136900643+water2027@users.noreply.github.com>
2025-02-08 23:58:09 +0800

    init

---
+++ uno.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig, presetAttributify, presetUno } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetAttributify({ /* preset 选项 */}),
+    presetUno(),
+    // ...自定义 presets
+  ],
+})
